f4318a6838b67e91d6e9e2ab85a9522f63461bc8
Dmitriy <devilins24@gmail.com>
2022-01-19 09:50:33 +0200

    add new comment func, style

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Comment from "./components/Comment";
 function App() {
   const [data, setData] = useState([]);
   const [currentUser, setCurrentUser] = useState();
+  const [commentValue, setCommentValue] = useState();
 
   const getData = async () => {
     const response = await fetch("./data/data.json");
@@ -37,6 +38,30 @@ function App() {
     setData(updatedComments);
   };
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    addComment();
+  };
+
+  function isNullOrWhiteSpace(str) {
+    return str == null || str.replace(/\s/g, "").length < 1;
+  }
+
+  const addComment = () => {
+    if (isNullOrWhiteSpace(commentValue)) return;
+    let newComment = {
+      id: Math.floor(Math.random() * 100) + 5,
+      createdAt: "1 week ago",
+      content: commentValue,
+      score: 0,
+      replies: [],
+      user: currentUser,
+    };
+    let newCommentsData = [...data, newComment];
+    setData(newCommentsData);
+    setCommentValue("");
+  };
+
   return (
     <div className="App">
       {data.map((comment) => (
@@ -49,6 +74,26 @@ function App() {
           updateScore={updateScore}
         />
       ))}
+      <section className="card add-comment-container">
+        <form onSubmit={submitHandler}>
+          <div className="add-comment__input">
+            <textarea
+              type="text"
+              name="reply-text"
+              id="reply-text"
+              value={commentValue}
+              onChange={(e) => setCommentValue(e.target.value)}
+              placeholder="Add a comment"
+            />
+          </div>
+          <div className="add-comment__bottom">
+            <img src={currentUser ? currentUser.image.png : null} alt="currentUser-pic" />
+            <button type="submit" className="btn add-comment__send-btn">
+              SEND
+            </button>
+          </div>
+        </form>
+      </section>
     </div>
   );
 }
